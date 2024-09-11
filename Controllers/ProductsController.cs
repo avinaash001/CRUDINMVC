ac131a6b9b9f66e1006b5825bc8f554ea71d1935
avinaash001 <avinaash001@gmail.com>
2024-09-11 14:54:21 +0530

    Add project files.

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,73 @@
+﻿using CRUDINMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+public class ProductsController : Controller
+{
+    private readonly ProductRepository _productRepository;
+
+    public ProductsController(ProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    public IActionResult Index()
+    {
+        var products = _productRepository.GetAll();
+        return View(products);
+    }
+
+    public IActionResult Details(int id)
+    {
+        var product = _productRepository.GetById(id);
+        return View(product);
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(Product product)
+    {
+        if (ModelState.IsValid)
+        {
+            _productRepository.Add(product);
+            return RedirectToAction(nameof(Index));
+        }
+        return View(product);
+    }
+
+    public IActionResult Edit(int id)
+    {
+        var product = _productRepository.GetById(id);
+        return View(product);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(Product product)
+    {
+        if (ModelState.IsValid)
+        {
+            _productRepository.Update(product);
+            return RedirectToAction(nameof(Index));
+        }
+        return View(product);
+    }
+
+    public IActionResult Delete(int id)
+    {
+        var product = _productRepository.GetById(id);
+        return View(product);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public IActionResult DeleteConfirmed(int id)
+    {
+        _productRepository.Delete(id);
+        return RedirectToAction(nameof(Index));
+    }
+}
---
+++ Data/ProductRepository.cs
@@ -0,0 +1,67 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using CRUDINMVC.Models;
+
+public class ProductRepository
+{
+    private readonly IConfiguration _configuration;
+    private readonly string _connectionString;
+
+    public ProductRepository(IConfiguration configuration)
+    {
+        _configuration = configuration;
+        _connectionString = _configuration.GetConnectionString("DefaultConnection");
+    }
+
+    public IDbConnection Connection
+    {
+        get { return new SqlConnection(_connectionString); }
+    }
+
+    public IEnumerable<Product> GetAll()
+    {
+        using (IDbConnection dbConnection = Connection)
+        {
+            string sql = "SELECT * FROM Products";
+            return dbConnection.Query<Product>(sql).ToList();
+        }
+    }
+
+    public Product GetById(int id)
+    {
+        using (IDbConnection dbConnection = Connection)
+        {
+            string sql = "SELECT * FROM Products WHERE Id = @Id";
+            return dbConnection.QuerySingleOrDefault<Product>(sql, new { Id = id });
+        }
+    }
+
+    public void Add(Product product)
+    {
+        using (IDbConnection dbConnection = Connection)
+        {
+            string sql = "INSERT INTO Products (Name, Price, Quantity) VALUES (@Name, @Price, @Quantity)";
+            dbConnection.Execute(sql, product);
+        }
+    }
+
+    public void Update(Product product)
+    {
+        using (IDbConnection dbConnection = Connection)
+        {
+            string sql = "UPDATE Products SET Name = @Name, Price = @Price, Quantity = @Quantity WHERE Id = @Id";
+            dbConnection.Execute(sql, product);
+        }
+    }
+
+    public void Delete(int id)
+    {
+        using (IDbConnection dbConnection = Connection)
+        {
+            string sql = "DELETE FROM Products WHERE Id = @Id";
+            dbConnection.Execute(sql, new { Id = id });
+        }
+    }
+}
